556f344fea2dc509399e546bae91d30ec87090ad
yoyounn88 <yoyounn88@gmail.com>
2024-08-16 15:48:09 +0900

    init: spring boot 프로젝트 생성

---
+++ src/main/java/com/kimyoungjo/spring_boot_first_practice/SpringBootFirstPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.kimyoungjo.spring_boot_first_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimyoungjo/spring_boot_first_practice/SpringBootFirstPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimyoungjo.spring_boot_first_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
