9a1cd4a954414b7db33c75fc810c9896ed2f1371
kimyoungjo <yoyounn88@gmail.com>
2024-08-19 15:27:08 +0900

    feat: 예외처리 경고문 클라이언트로 보내는 클래스 작성

--- src/main/java/com/kimyoungjo/spring_boot_first_practice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kimyoungjo/spring_boot_first_practice/exception/GlobalExceptionHandler.java
@@ -1,2 +1,16 @@
-package com.kimyoungjo.spring_boot_first_practice.exception;public class GlobalExceptionHandler {
+package com.kimyoungjo.spring_boot_first_practice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
 }
