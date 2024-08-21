bcce4958715a3cf1ff695747ad915e98555a897a
kimyoungjo <yoyounn88@gmail.com>
2024-08-21 10:25:39 +0900

    feat: Comment model 구현

--- src/main/java/com/kimyoungjo/spring_boot_first_practice/model/Comment.java
+++ src/main/java/com/kimyoungjo/spring_boot_first_practice/model/Comment.java
@@ -1,4 +1,44 @@
 package com.kimyoungjo.spring_boot_first_practice.model;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "comment_id")
+    private String commentId;
+
+    @ManyToOne // 다대일 관계 명시
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Comment(Post post, User user, String content) {
+        this.post = post;
+        this.user = user;
+        this.content = content;
+    } // 필수 요소 커스텀 생성자 명시
 }
