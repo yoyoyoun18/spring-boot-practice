fc92ad04b128e521e083f1bb47242ffdf64133a9
kimyoungjo <yoyounn88@gmail.com>
2024-08-21 10:22:49 +0900

    feat: Post model 구현

--- src/main/java/com/kimyoungjo/spring_boot_first_practice/model/Post.java
+++ src/main/java/com/kimyoungjo/spring_boot_first_practice/model/Post.java
@@ -1,4 +1,52 @@
 package com.kimyoungjo.spring_boot_first_practice.model;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "post_id")
+    private String postId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false) // FK 조인
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT") // 더 큰 텍스트 저장 할 수 있게 함
+    private String content;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments;
+
+    @Builder
+    public Post(User user, String title, String content) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+    }
 }
