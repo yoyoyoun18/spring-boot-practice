30d9ec9686af4b37064c4e51c108c21352b12ef0
kimyoungjo <yoyounn88@gmail.com>
2024-08-21 10:07:29 +0900

    feat: User model 작성

--- src/main/java/com/kimyoungjo/spring_boot_first_practice/model/User.java
+++ src/main/java/com/kimyoungjo/spring_boot_first_practice/model/User.java
@@ -1,4 +1,42 @@
 package com.kimyoungjo.spring_boot_first_practice.model;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) // 유저 고유의 랜덤 난수 id를 만들어주기위해 UUID 채택
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    } // 유저 정보는 저 3가지가 필수니까 필수 요소를 지정하기위해 커스텀 생성자 만듦
 }
