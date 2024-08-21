4ab5886dd555a88fd64bb05e3eb4dcc04426dc96
kimyoungjo <yoyounn88@gmail.com>
2024-08-21 13:27:05 +0900

    feat: jpa를 상속받은 UserRepository 구현 후 옵셔널 객체 설정

--- src/main/java/com/kimyoungjo/spring_boot_first_practice/repository/UserRepository.java
+++ src/main/java/com/kimyoungjo/spring_boot_first_practice/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.kimyoungjo.spring_boot_first_practice.repository;
 
-public interface UserRepository {
+import com.kimyoungjo.spring_boot_first_practice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
