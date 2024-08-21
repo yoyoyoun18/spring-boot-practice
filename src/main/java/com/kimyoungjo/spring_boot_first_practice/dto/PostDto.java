deecb025bc28d42094079595f761d467994471de
kimyoungjo <yoyounn88@gmail.com>
2024-08-21 12:29:33 +0900

    feat: PostDTO 구현

--- src/main/java/com/kimyoungjo/spring_boot_first_practice/dto/PostDto.java
+++ src/main/java/com/kimyoungjo/spring_boot_first_practice/dto/PostDto.java
@@ -1,4 +1,29 @@
 package com.kimyoungjo.spring_boot_first_practice.dto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 public class PostDto {
+    @Getter
+    @Setter
+    @Builder
+    public static class Request {
+        private String title;
+        private String content;
+        private String userId;  // 포스트를 작성하는 사용자의 ID
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    public static class Response {
+        private String postId;
+        private String title;
+        private String content;
+        private String username;  // 작성자 이름
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+    }
 }
