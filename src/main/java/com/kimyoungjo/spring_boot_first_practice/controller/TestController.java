a86cc157d6e17f49331387883546d00c9f85baf9
kimyoungjo <yoyounn88@gmail.com>
2024-08-26 16:07:52 +0900

    init: TestController 작성 후 Hello World 출력

--- src/main/java/com/kimyoungjo/spring_boot_first_practice/controller/TestController.java
+++ src/main/java/com/kimyoungjo/spring_boot_first_practice/controller/TestController.java
@@ -1,2 +1,12 @@
-package com.kimyoungjo.spring_boot_first_practice.controller;public class TestController {
+package com.kimyoungjo.spring_boot_first_practice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World!";
+    }
 }
